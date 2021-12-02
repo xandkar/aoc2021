a1b76222b3bb3d7082890389d8766109293f857f
Siraaj Khandkar <siraaj@khandkar.net>
2021-12-02 15:16:00 -0500

    Refactor to use a struct per instruction
    a more-Rackety way to represent sum type variants.

--- 02.rkt
+++ 02.rkt
@@ -1,102 +1,91 @@
 #lang racket
 
-(define dir? (or/c 'forward 'down 'up))
+(struct Up (magnitude) #:transparent)
+(struct Down (magnitude) #:transparent)
+(struct Forward (magnitude) #:transparent)
 
-(struct Ins (dir mag) #:transparent)
+(define Instruction/c
+  (or/c (struct/dc Up [magnitude exact-nonnegative-integer?])
+        (struct/dc Down [magnitude exact-nonnegative-integer?])
+        (struct/dc Forward [magnitude exact-nonnegative-integer?])))
 
-(define Ins/c
-  (struct/dc Ins
-             [dir dir?]
-             [mag exact-nonnegative-integer?]))
-
-(define instructions? (listof Ins/c))
+(define Instructions/c (listof Instruction/c))
 
 (struct Pos (horiz depth aim) #:transparent)
-
 (define Pos/c
   (struct/dc Pos
              [horiz exact-integer?]
              [depth exact-integer?]
              [aim   exact-integer?]))
 
-(define/contract (str->dir s)
-  (-> string? dir?)
-  (match s
-    ["forward" 'forward]
-    ["down" 'down]
-    ["up" 'up]))
-
-(define/contract (str->mag s)
-  (-> string? exact-nonnegative-integer?)
-  (string->number s))
-
 (define/contract (str->ins s)
-  (-> string? Ins/c)
+  (-> string? Instruction/c)
   (match (string-split s)
-    [(list s1 s2) (Ins (str->dir s1) (str->mag s2))]
-    [_ (raise (cons 'invalid-instruction-data s))]))
+    [(list "up" n) (Up (string->number n))]
+    [(list "down" n) (Down (string->number n))]
+    [(list "forward" n) (Forward (string->number n))]))
 
-(define/contract (file->inss path)
-  (-> path-string? instructions?)
+(define/contract (file->instructions path)
+  (-> path-string? Instructions/c)
   (map str->ins (file->lines path)))
 
 (define/contract (move-v1 i p)
-  (-> Ins/c Pos/c Pos/c)
+  (-> Instruction/c Pos/c Pos/c)
   (match i
-    [(Ins 'forward m) (struct-copy Pos p [horiz (+ (Pos-horiz p) m)])]
-    [(Ins 'down    m) (struct-copy Pos p [depth (+ (Pos-depth p) m)])]
-    [(Ins 'up      m) (struct-copy Pos p [depth (- (Pos-depth p) m)])]))
+    [(Up      m) (struct-copy Pos p [depth (- (Pos-depth p) m)])]
+    [(Down    m) (struct-copy Pos p [depth (+ (Pos-depth p) m)])]
+    [(Forward m) (struct-copy Pos p [horiz (+ (Pos-horiz p) m)])]))
 
 (define/contract (move-v2 i p)
-  (-> Ins/c Pos/c Pos/c)
+  (-> Instruction/c Pos/c Pos/c)
   (match i
-    [(Ins 'down    m) (struct-copy Pos p [aim (+ (Pos-aim p) m)])]
-    [(Ins 'up      m) (struct-copy Pos p [aim (- (Pos-aim p) m)])]
-    [(Ins 'forward m) (struct-copy Pos p
-                                   [horiz (+ (Pos-horiz p) m)]
-                                   [depth (+ (Pos-depth p) (* m (Pos-aim p)))])]))
+    [(Up      m) (struct-copy Pos p [aim (- (Pos-aim p) m)])]
+    [(Down    m) (struct-copy Pos p [aim (+ (Pos-aim p) m)])]
+    [(Forward m) (struct-copy Pos p
+                              [horiz (+ (Pos-horiz p) m)]
+                              [depth (+ (Pos-depth p) (* m (Pos-aim p)))])]))
 
 (define/contract (interpret-v1 instructions pos)
-  (-> instructions? Pos/c Pos/c)
+  (-> Instructions/c Pos/c Pos/c)
   (foldl move-v1 pos instructions))
 
 (define/contract (interpret-v2 instructions pos)
-  (-> instructions? Pos/c Pos/c)
+  (-> Instructions/c Pos/c Pos/c)
   (foldl move-v2 pos instructions))
 
 (define/contract (solve interpret instructions state)
-  (-> (-> instructions? Pos/c Pos/c) instructions? Pos/c exact-integer?)
+  (-> (-> Instructions/c Pos/c Pos/c) Instructions/c Pos/c exact-integer?)
   (match-define (Pos h d _) (interpret instructions state))
   (* h d))
 
 (define/contract (part-1 instructions)
-  (-> instructions? exact-integer?)
+  (-> Instructions/c exact-integer?)
   (solve interpret-v1 instructions (Pos 0 0 0)))
 
 (define/contract (part-2 instructions)
-  (-> instructions? exact-integer?)
+  (-> Instructions/c exact-integer?)
   (solve interpret-v2 instructions (Pos 0 0 0)))
 
 (module+ test
   (require rackunit)
 
-  (define data-example (file->inss "data/02/example.txt"))
-  (define data-input   (file->inss "data/02/input.txt"))
+  (define data-example (file->instructions "data/02/example.txt"))
+  (define data-input   (file->instructions "data/02/input.txt"))
 
-  (check-equal? (move-v1 (Ins 'up 1) (Pos 0 0 0)) (Pos 0 -1 0))
-  (check-equal? (move-v1 (Ins 'down 1) (Pos 0 0 0)) (Pos 0 1 0))
-  (check-equal? (move-v1 (Ins 'forward 1) (Pos 0 0 0)) (Pos 1 0 0))
+  (check-equal? (move-v1 (Up 1) (Pos 0 0 0)) (Pos 0 -1 0))
+  (check-equal? (move-v1 (Down 1) (Pos 0 0 0)) (Pos 0 1 0))
+  (check-equal? (move-v1 (Forward 1) (Pos 0 0 0)) (Pos 1 0 0))
 
-  (check-equal? (move-v2 (Ins 'up 1) (Pos 0 0 0)) (Pos 0 0 -1))
-  (check-equal? (move-v2 (Ins 'down 1) (Pos 0 0 0)) (Pos 0 0 1))
-  (check-equal? (move-v2 (Ins 'forward 1) (Pos 0 0 0)) (Pos 1 0 0))
+  (check-equal? (move-v2 (Up 1) (Pos 0 0 0)) (Pos 0 0 -1))
+  (check-equal? (move-v2 (Down 1) (Pos 0 0 0)) (Pos 0 0 1))
+  (check-equal? (move-v2 (Forward 1) (Pos 0 0 0)) (Pos 1 0 0))
   (check-equal? (interpret-v2
-                  (list (Ins 'forward 5)
-                        (Ins 'down 5)
-                        (Ins 'forward 8)
-                        (Ins 'up 3)
-                        (Ins 'down 8)
-                        (Ins 'forward 2))
+                  (list (Forward 5)
+                        (Down 5)
+                        (Forward 8)
+                        (Up 3)
+                        (Down 8)
+                        (Forward 2))
                   (Pos 0 0 0))
                 (Pos 15 60 10))
 
